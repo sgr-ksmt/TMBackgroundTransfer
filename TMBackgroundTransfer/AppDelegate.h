8414e3144c4e107273b235b2ae5866f5726b9cbe
1_am_a_geek <tmy0x3@icloud.com>
2015-11-27 15:06:51 +0900

    Initial Commit

---
+++ TMBackgroundTransfer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMBackgroundTransfer
+//
+//  Created by 1amageek on 2015/11/27.
+//  Copyright © 2015年 Timers inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TMBackgroundTransfer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMBackgroundTransfer
+//
+//  Created by 1amageek on 2015/11/27.
+//  Copyright © 2015年 Timers inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
